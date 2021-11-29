c8de4fdadb5d463810bed9d36a106eb4e62379c9
Anushree Abhyankar <anush.abhyankar@gmail.com>
2021-11-29 18:43:22 -0500

    Add category selection feature

--- website/main.js
+++ website/main.js
@@ -1,4 +1,12 @@
-onSelectCategory = (element) => {
-    element.classList.toggle("selected-pill");
-
+let selectedCategories = [];
+const onSelectCategory = (element) => {
+    selectedCategories.push(element.attributes.value.value);
 }
+
+const onLoadBody = () => {
+    let fileData = null;
+    fetch("data.json").then(response => response.json()).then(data => {
+        fileData = data;
+    })
+}
+
