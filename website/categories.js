f65d1c6d70af463a85e63dc58f5fc1e239469f46
Anushree Abhyankar <anush.abhyankar@gmail.com>
2021-11-30 16:28:53 -0500

    Create categories dynamically based on selections

---
+++ website/categories.js
@@ -0,0 +1,17 @@
+const selectedCategories = JSON.parse(localStorage.getItem('selectedCategories'));
+
+const categoriesContainerEl = document.getElementById("category-blocks");
+
+if (selectedCategories.length) {
+    selectedCategories.forEach(category => {
+        const categoryBlockDivEl = document.createElement("div");
+        categoryBlockDivEl.classList.add("category-block-link");
+        const categoryBlockLinkEl = document.createElement("a");
+        categoryBlockLinkEl.classList.add("category-block")
+        categoryBlockLinkEl.innerHTML = category;
+        categoryBlockLinkEl.href = "links.html";
+        categoryBlockDivEl.appendChild(categoryBlockLinkEl)
+
+        categoriesContainerEl.appendChild(categoryBlockDivEl);
+    });
+}
