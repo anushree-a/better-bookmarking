97244623c18e0bf0a9d095d7a758bfd68baf45b2
Anushree Abhyankar <anush.abhyankar@gmail.com>
2021-11-30 12:05:55 -0500

    Add category to local storage on selection

--- website/main.js
+++ website/main.js
@@ -1,6 +1,8 @@
 let selectedCategories = [];
 const onSelectCategory = (element) => {
+    element.classList.toggle("selected-pill");
     selectedCategories.push(element.attributes.value.value);
+    localStorage.setItem('selectedCategories', JSON.stringify(selectedCategories));
 }
 
 const onLoadBody = () => {
