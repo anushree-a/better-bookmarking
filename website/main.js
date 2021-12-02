295d6658a777bb5f7f537d005358a6ab971c2bc4
Anushree Abhyankar <anush.abhyankar@gmail.com>
2021-12-02 16:52:50 -0500

    Allow user to create custom categories

--- website/main.js
+++ website/main.js
@@ -1,6 +1,30 @@
 let selectedCategories = [];
 const onSelectCategory = (element) => {
-    element.classList.toggle("selected-pill");
-    selectedCategories.push(element.attributes.value.value);
+    console.log(element)
+    if (element.target) {
+        selectedCategories.push(element.target.value);
+        element.target.classList.toggle("selected-pill");
+    }
+    else {
+        selectedCategories.push(element.attributes.value.value);
+        element.classList.toggle("selected-pill");
+    }
     localStorage.setItem('selectedCategories', JSON.stringify(selectedCategories));
+}
+
+
+const onCreateCategoryBtnClick = () => {
+    const newCategoryValue = document.getElementById("additional-category-input").value;
+    const categoryParentDiv = document.getElementById("bookmark pills");
+    const newCategoryPill = document.createElement("div");
+    newCategoryPill.classList.add("category-pill")
+    newCategoryPill.value = newCategoryValue;
+    newCategoryPill.innerHTML = newCategoryValue;
+    newCategoryPill.id = "x"
+
+    categoryParentDiv.appendChild(newCategoryPill);
+
+    document.getElementById("x").addEventListener('click', onSelectCategory.bind(newCategoryPill))
+
+    document.getElementById("additional-category-input").value = "";
 }
