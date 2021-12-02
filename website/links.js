2e8a9b9c169a8b81cddd25cfb406e3301fab7732
Anushree Abhyankar <anush.abhyankar@gmail.com>
2021-12-02 17:02:27 -0500

    Add 0 search results state

--- website/links.js
+++ website/links.js
@@ -4,25 +4,30 @@ const renderLinks = (linkObj) => {
     const tableEl = document.getElementById("links");
     tableEl.innerHTML = null;
 
-    linkObj.forEach(l => {
-        const linkRowEl = document.createElement("tr");
-        const linkValueEl = document.createElement("td")
-        const linkValue = document.createElement("a");
-        linkValue.href = l.link;
-        linkValue.target = "_blank"
-        linkValue.innerHTML = l.link;
-        linkValueEl.appendChild(linkValue);
-        const linkNameEl = document.createElement("td")
-        linkNameEl.innerHTML = l.recommender
-        const linkNotesEl = document.createElement("td")
-        linkNotesEl.innerHTML = l.notes
+    if (linkObj.length) {
+        linkObj.forEach(l => {
+            const linkRowEl = document.createElement("tr");
+            const linkValueEl = document.createElement("td")
+            const linkValue = document.createElement("a");
+            linkValue.href = l.link;
+            linkValue.target = "_blank"
+            linkValue.innerHTML = l.link;
+            linkValueEl.appendChild(linkValue);
+            const linkNameEl = document.createElement("td")
+            linkNameEl.innerHTML = l.recommender
+            const linkNotesEl = document.createElement("td")
+            linkNotesEl.innerHTML = l.notes
 
-        linkRowEl.appendChild(linkValueEl);
-        linkRowEl.appendChild(linkNameEl)
-        linkRowEl.appendChild(linkNotesEl);
+            linkRowEl.appendChild(linkValueEl);
+            linkRowEl.appendChild(linkNameEl)
+            linkRowEl.appendChild(linkNotesEl);
 
-        tableEl.appendChild(linkRowEl);
-    });
+            tableEl.appendChild(linkRowEl);
+        });
+    }
+    else {
+        tableEl.innerHTML = "Oops! Looks like you haven't saved anything like that yet. 😕"
+    }
 }
 
 fetch("data.json").then(response => response.json()).then(linkData => {
